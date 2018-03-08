39f9cb757e8c3722ab6764d7b6812326e159fa8b
aish25 <36500584+aish25@users.noreply.github.com>
2018-03-08 16:14:41 +0100

    Add files via upload

--- train-predict.py
+++ train-predict.py
@@ -16,81 +16,81 @@ def read(y):
     i=0
     for line in file_2:
         
-         #if line.startswith('>'):
-          #   d = line.split('>')
-           #  ids.append(d)
-            # sequence.append(file_2[i+1])
-             #topology.append(file_2[i+2])
-             #i=i+1
-         #else:
-          #   i=i+1
+         if line.startswith('>'):
+             d = line.split('>')
+             ids.append(d)
+             sequence.append(file_2[i+1])
+             topology.append(file_2[i+2])
+             i=i+1
+         else:
+             i=i+1
    
-    #dict_top ={'I':1, 'M':2, 'O':3, 'G':4}
-    #new_label = []
-    #for al in topology:
-     #   splitting= list(al)
-      #  label =[]
-       # for val in splitting:
-        #    for key,value in dict_top.items():
+    dict_top ={'I':1, 'M':2, 'O':3, 'G':4}
+    new_label = []
+    for al in topology:
+        splitting= list(al)
+        label =[]
+        for val in splitting:
+            for key,value in dict_top.items():
                 
-         #       if val == key:
-          #          label.append(value)
-        #new_label.extend(label)
+                if val == key:
+                    label.append(value)
+        new_label.extend(label)
    
-    #y = np.array(new_label)
+    y = np.array(new_label)
     
     
-    #print(y.shape)
-    #zero_array = np.zeros(shape=(20,20),dtype=int)
-    #np.fill_diagonal(zero_array,1)
-    #zero_1 = zero_array.tolist()
+    print(y.shape)
+    zero_array = np.zeros(shape=(20,20),dtype=int)
+    np.fill_diagonal(zero_array,1)
+    zero_1 = zero_array.tolist()
     
     
-    #dict_con = {}
-    #for zero_array, acid in zip(zero_1,amino_list):
-     #   dict_con[acid]=zero_array
+    dict_con = {}
+    for zero_array, acid in zip(zero_1,amino_list):
+        dict_con[acid]=zero_array
     
-    #for window_size in range(3,50):
-        #print(window_size)
-     #   pad =int(window_size//2)
+    for window_size in range(3,50):
+        print(window_size)
+        pad =int(window_size//2)
         
-      #  pad_list = []
-       # for seq in sequence:
-        #    seq_list = []
-         #   new_sequence =(pad*'0'+seq+pad*'0')
-          #  seq_list.append(new_sequence)
-           # pad_list.extend(seq_list)
+        pad_list = []
+        for seq in sequence:
+            seq_list = []
+            new_sequence =(pad*'0'+seq+pad*'0')
+            seq_list.append(new_sequence)
+            pad_list.extend(seq_list)
             
-        #list_of_window = []
-        #for element in pad_list:
-         #   for value in range(0,len(element)):
+        list_of_window = []
+        for element in pad_list:
+            for value in range(0,len(element)):
             
-          #      if (value+window_size)<=len(element):
-           #         triple = element[value:value+(window_size)]
-            #        list_of_window.append(triple)
+                if (value+window_size)<=len(element):
+                    triple = element[value:value+(window_size)]
+                    list_of_window.append(triple)
                  
              
-    #dict_con.update({'0':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]})
+    dict_con.update({'0':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]})
     
-    #read_window = []
-    #for w in list_of_window:
-     #   nw_list = []
-      #  for alphabet in w:
-       #     for key,value in dict_con.items():
-        #        if alphabet == key:
-         #           nw_list.extend(value)
-        #read_window.append(nw_list)
+    read_window = []
+    for w in list_of_window:
+        nw_list = []
+        for alphabet in w:
+            for key,value in dict_con.items():
+                if alphabet == key:
+                    nw_list.extend(value)
+        read_window.append(nw_list)
     #print(read_window)
-    #x= np.array(read_window)
-    #test = te.reading("test.txt")
-    #print(test)
+    x= np.array(read_window)
+    test = te.reading("test.txt")
+    print(test)
     
     #print(test.shape)
     #print(x.shape)
     #print(y.shape)
-    #classify = svm.SVC(kernel = 'linear', C=1)
-    #scores = cross_val_score(classify,x,y,cv=5)
-    #print("scores:", scores)
+    classify = svm.SVC(kernel = 'linear', C=1)
+    scores = cross_val_score(classify,x,y,cv=5)
+    print("scores:", scores)
     #classify.fit(x,y)
     #c = classify.predict(test)
     #top_predict = []
@@ -116,5 +116,5 @@ def read(y):
          
     #file_1.close()
 if __name__ == "__main__":
-	read("seq-data.txt")
+     read("seq-data.txt")
     
